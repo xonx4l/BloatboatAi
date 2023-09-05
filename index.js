01856178d3755d75db83662799c1c5fdc9d89905
xonx4l <sk797119@gmail.com>
2023-09-05 11:28:38 +0530

    ok

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import {Authorisation} from Authorisation;
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "YOUR_ORG_ID",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [
